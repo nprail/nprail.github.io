cd07d995a49700f61ec4567dab87bc050d12e475
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-14 14:40:41 +0000

    Deploy nprail/nprail.github.io to github.com/nprail/nprail.github.io.git:master

---
+++ precache-manifest.b4b8f49ac396cb2d0e41a9e7692b9353.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468d54fb712e3552445780fe7be3f75a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2115e5072b5ba9c18301",
+    "url": "/static/css/main.a01901a0.chunk.css"
+  },
+  {
+    "revision": "6b2a80efd6684b2c9e3b",
+    "url": "/static/js/2.2f89013e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f89013e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2115e5072b5ba9c18301",
+    "url": "/static/js/main.6edffdaf.chunk.js"
+  },
+  {
+    "revision": "98bf03d62f44ee2bec63",
+    "url": "/static/js/runtime-main.721ed664.js"
+  }
+]);
