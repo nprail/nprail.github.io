a4b64817f7dc9b0d09a008fdcf339905a485b88c
Noah Prail <nprail@event1.io>
2026-05-29 10:23:57 -0500

    chore: Add sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,20 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addCollection("sitemap", (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob("./src/**/*.njk")
+      .filter((item) => item.url && item.url !== "/sitemap.xml");
+  });
 
   return {
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes",
-      data: "_data"
+      data: "_data",
     },
     templateFormats: ["njk", "html", "md"],
     htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk"
+    markdownTemplateEngine: "njk",
   };
 };
